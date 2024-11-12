28c0b3f1ca7e6decc07ae0031b7aa2911136b063
MatiasFilliez <matiifilliez@gmail.com>
2024-11-12 00:19:57 -0300

    ajustes de parmetros de la db

--- src/db.js
+++ src/db.js
@@ -5,34 +5,35 @@ import { Sequelize } from 'sequelize'
 
 configDotenv()
 const {
-    DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME,
 } = process.env;
-
+console.log(DB_NAME, DB_USER, DB_PASSWORD)
 const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/mutantChallenge`,
-    {
-      logging: false,
-      native: false,
-    }
-  );
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: 'postgres',
+}
+);
 
 const modelDefined = [
-    mutanModel,
-    statsModel
+  mutanModel,
+  statsModel
 ]
 
-modelDefined.forEach((model)=> model(sequelize))
+modelDefined.forEach((model) => model(sequelize))
 
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-const {Dna, Stats} = sequelize.models
+const { Dna, Stats } = sequelize.models
 
 const connect = sequelize
 
 export {
-    connect,
-    Stats,
-    Dna
+  connect,
+  Stats,
+  Dna
 }
